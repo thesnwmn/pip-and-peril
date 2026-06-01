4b6bac1e442acf115c5aef80639ca134a5a7c909
thesnwmn <thesnwmn@users.noreply.github.com>
2026-06-01 09:19:08 +0000

    Deploying to gh-pages from @ thesnwmn/pip-and-peril@cd245c11b0d9140f6e44659c1d8a546222e35d17 🚀

--- pr-6/src/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-describe('main', () => {
-  it('should pass a trivial test', () => {
-    expect(true).toBe(true)
-  })
-})
--- pr-6/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  build: { outDir: 'dist' },
-  test: { environment: 'jsdom' }
-})
